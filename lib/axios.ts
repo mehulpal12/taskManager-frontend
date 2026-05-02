3ecb8801b8636110823212412ff02b12c988fd15
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-05-02 14:52:05 +0530

    url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-backend-nqqe.vercel.app/api"
+  baseURL: "https://taskmanagerbackend-uc0i.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
